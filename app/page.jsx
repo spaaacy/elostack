0105004a0689d9bf92198d0beeed4fcfbeaa09f0
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-05-24 16:04:00 -0400

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <NavBar />
       <div className="flex flex-col justify-center items-center">
         <p className="mx-auto text-lg">Site under construction! Join the community now.</p>
-        <Link className="mt-4" href={"https://discord.gg/Re8W4Kp5ae" target="_blank"}>
+        <Link className="mt-4" href={"https://discord.gg/Re8W4Kp5ae"} target="_blank">
           <Image src={"/discord.png"} alt="discord" width={40} height={40} />
         </Link>
       </div>
