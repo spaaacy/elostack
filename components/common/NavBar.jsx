9b2c0b5618107f3287eaa64a395da77f1a04057a
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-05-28 11:55:18 -0400

    Added mobile nav bar

--- components/common/NavBar.jsx
+++ components/common/NavBar.jsx
@@ -7,12 +7,14 @@ import { usePathname, useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "@/context/UserContext";
 import { supabase } from "@/utils/supabase";
+import { IoMenu } from "react-icons/io5";
 const kanit = Kanit({ subsets: ["latin"], weight: "600" });
 
 const NavBar = () => {
   const { session } = useContext(UserContext);
   const router = useRouter();
   const [user, setUser] = useState();
+  const [showMobileNav, setShowMobileNav] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -53,43 +55,67 @@ const NavBar = () => {
   const showSignIn = pathname !== "/signin" && pathname !== "/signup";
 
   return (
-    <nav className="px-16 py-2 flex items-center justify-start">
-      <Link href={"/"} className={`${kanit.className} flex justify-center items-center text-2xl`}>
-        <Image src={"/logo.png"} alt={"logo"} width={50} height={50} />
-        EloStack
-      </Link>
-      <div className="ml-12 flex justify-center items-center gap-4">
-        <Link
-          href={"/projects"}
-          className="p-2 border-b-2 border-transparent hover:border-b-gray-300 hover:text-gray-300"
-        >
-          Projects
+    <nav>
+      <div className="px-8 md:px-16 py-2 flex items-center justify-start">
+        <Link href={"/"} className={`${kanit.className} flex justify-center items-center text-2xl flex-shrink-0`}>
+          <Image src={"/logo.png"} alt={"logo"} width={50} height={50} />
+          <span className="max-sm:hidden">EloStack</span>
         </Link>
-        <Link
-          href={"/create-project"}
-          className="p-2 border-b-2 border-transparent hover:border-b-gray-300 hover:text-gray-300"
-        >
-          Create Project
-        </Link>
+        <DesktopNav showSignIn={showSignIn} signOut={signOut} session={session} />
+        <button type="button" className="sm:hidden  ml-auto" onClick={() => setShowMobileNav(!showMobileNav)}>
+          <IoMenu className="text-2xl" />
+        </button>
       </div>
-      {showSignIn &&
-        (session?.data.session ? (
-          <button
-            onClick={signOut}
-            className="hover:bg-gray-600 hover:bg-opacity-50 text-gray-200 px-3 py-1 rounded-full ml-auto text-sm"
-          >
-            Log out
-          </button>
-        ) : (
-          <Link
-            href={"/signin"}
-            className="hover:bg-gray-600 hover:bg-opacity-50 text-gray-200 px-3 py-1 rounded-full ml-auto text-sm"
-          >
-            Sign In
-          </Link>
-        ))}
+      {showMobileNav && <MobileNav showSignIn={showSignIn} signOut={signOut} session={session} />}
     </nav>
   );
 };
 
 export default NavBar;
+
+const DesktopNav = ({ showSignIn, signOut, session }) => {
+  return (
+    <div className="ml-12 flex justify-start items-center gap-4 max-sm:hidden w-full">
+      <Link
+        href={"/projects"}
+        className="p-2 border-b-2 border-transparent hover:border-b-gray-300 hover:text-gray-300"
+      >
+        Projects
+      </Link>
+      <Link
+        href={"/create-project"}
+        className="p-2 border-b-2 border-transparent hover:border-b-gray-300 hover:text-gray-300"
+      >
+        Create Project
+      </Link>
+      {showSignIn && (
+        <div
+          className="hover:bg-gray-600 hover:bg-opacity-50 text-gray-200 px-3 py-1 rounded-full ml-auto
+          text-sm"
+        >
+          {session?.data.session ? <button onClick={signOut}>Log out</button> : <Link href={"/signin"}>Sign In</Link>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const MobileNav = ({ showSignIn, signOut, session }) => {
+  const [showSidebar, setShowSidebar] = useState();
+  return (
+    <div className="flex flex-col justify-center items-center sm:hidden">
+      <Link href={"/projects"} className="p-2 hover:bg-gray-600 hover:bg-opacity-50 w-full text-center">
+        Projects
+      </Link>
+      <Link href={"/create-project"} className="p-2 hover:bg-gray-600 hover:bg-opacity-50 w-full text-center">
+        Create Project
+      </Link>
+      <hr className="border-0 h-[1px] bg-gray-400 my-2 w-full" />
+      {showSignIn && (
+        <div className="p-2 hover:bg-gray-600 hover:bg-opacity-50 w-full text-center">
+          {session?.data.session ? <button onClick={signOut}>Log out</button> : <Link href={"/signin"}>Sign In</Link>}
+        </div>
+      )}
+    </div>
+  );
+};
