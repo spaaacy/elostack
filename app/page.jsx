96de37df301ee41c1fa28c0c40d326bb76c0caec
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-05-31 18:20:22 -0400

    attempt at fixing iphone ui for landing page

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Page = () => {
         <NavBar />
         <Image className="-z-50 object-cover" src={"/astronaut.jpg"} alt="hero_bg" fill={true} />
         <div
-          className={`max-2xl:px-8 absolute 2xl:right-72 max-2xl:right-0 max-2xl:bottom-1/4 2xl:top-1/2 2xl:-translate-y-1/2 transform text-right flex flex-col items-end justify-center  ${jetbrainsMono.className}`}
+          className={`max-2xl:px-8 absolute 2xl:right-72 max-2xl:right-0 max-2xl:bottom-28 2xl:top-1/2 2xl:-translate-y-1/2 transform text-right flex flex-col items-end justify-center  ${jetbrainsMono.className}`}
         >
           <h1 className="text-4xl md:text-5xl ">
             Don't go alone...
