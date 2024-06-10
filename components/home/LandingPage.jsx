1a161e40879bd5261d536c5bac7f8a7f06847634
spaaacy <90762160+spaaacy@users.noreply.github.com>
2024-06-10 12:30:53 -0400

    Added light mode for home page

--- components/home/LandingPage.jsx
+++ components/home/LandingPage.jsx
@@ -53,14 +53,14 @@ const LandingPage = () => {
   }, [isCursorInside]);
 
   return (
-    <div className="flex flex-col dark text-white bg-black">
+    <div className="flex flex-col ">
       <section className="h-[calc(100dvh)] z-10 relative">
         <div className="absolute bg-black h-full w-full opacity-30 -z-40 md:hidden"></div>
         <NavBar />
         <Image className="-z-50 object-cover h-full" src={"/astronaut.jpg"} alt="hero_bg" fill={true} unoptimized />
         <div className="absolute bottom-0 left-0 h-1/3 w-full -z-40 bg-gradient-to-b from-transparent to-black"></div>
         <div
-          className={`max-2xl:px-8 absolute 2xl:right-72 max-2xl:right-0 max-sm:bottom-[10%] sm:top-1/2 2xl:-translate-y-1/2 transform text-right flex flex-col items-end justify-center  ${jetbrainsMono.className}`}
+          className={`text-white max-2xl:px-8 absolute 2xl:right-72 max-2xl:right-0 max-sm:bottom-[10%] sm:top-1/2 2xl:-translate-y-1/2 transform text-right flex flex-col items-end justify-center  ${jetbrainsMono.className}`}
         >
           <h1 className="max-sm:text-3xl sm:text-4xl md:text-5xl ">
             Don't go alone...
@@ -73,7 +73,7 @@ const LandingPage = () => {
           <Link
             href={"/projects"}
             className={
-              "p-4 md:text-opacity-80 hover:text-opacity-100 text-white bg-black mt-8 md:bg-opacity-60 hover:bg-opacity-100"
+              "p-4 md:text-opacity-90 hover:text-opacity-100 text-black bg-white mt-8 md:bg-opacity-90 hover:bg-opacity-100"
             }
           >
             Discover projects
@@ -81,10 +81,10 @@ const LandingPage = () => {
         </div>
       </section>
 
-      <div className=" bg-gradient-to-tr to-slate-800 from-black relative">
+      <div className=" bg-gradient-to-t to-slate-600 from-backgroundlight dark:to-slate-700 dark:from-backgrounddark relative">
         <div className="absolute top-0 left-0 h-[200px] w-full z-60 bg-gradient-to-t from-transparent to-black"></div>
-        <div className="absolute bottom-0 left-0 h-1/6 w-full z-40 bg-gradient-to-b  from-transparent to-black"></div>
-        <section ref={sectionRef} className="z-10 py-24 sm:py-32 lg:py-48">
+        {/* <div className="absolute bottom-0 left-0 h-1/6 w-full z-40 bg-gradient-to-b  from-transparent to-black"></div> */}
+        <section ref={sectionRef} className="z-10 py-24 sm:py-32 lg:py-48 mt-32">
           <div className="flex flex-col lg:flex-row gap-20 sm:gap-32 lg:gap-44  items-center justify-center w-3/4 text-left mx-auto ">
             <div
               ref={projectRef}
@@ -109,7 +109,7 @@ const LandingPage = () => {
               </div>
             </div>
 
-            <div className="lg:w-1/2">
+            <div className="lg:w-1/2 text-white">
               <h4 className={`text-4xl ${montserrat.className}`}>
                 What is <span className={`${kanit.className}`}>EloStack</span>?
               </h4>
@@ -133,9 +133,9 @@ const LandingPage = () => {
           <div
             className={`absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col items-center justify-center  w-full  ${cormorant.className}`}
           >
-            <div className="max-sm:h-72 h-1/3 w-3/4 xl:w-1/2 flex flex-col justify-center items-center py-16  px-8 relative rounded">
+            <div className="max-sm:h-72 h-1/3 w-3/4 xl:w-1/2 flex flex-col justify-center items-center py-16  px-8 relative rounded text-white">
               <Image className="-z-50 object-cover rounded-[20px]" src={"/wormhole.jpg"} alt="wormhole" fill={true} />
-              <div className="absolute top-0 bottom-0 h-full w-full opacity-50 -z-40 bg-black"></div>
+              <div className="absolute top-0 bottom-0 h-full w-full opacity-50 -z-40 bg-black rounded-[20px]"></div>
               <h3 className="max-sm:text-2xl sm:text-4xl md:text-5xl">No ideas? No problem.</h3>
 
               <p className={"max-sm:text-xl sm:text-3xl md:text-4xl mt-4 sm:mt-6 md:mt-8"}>
@@ -160,7 +160,7 @@ const LandingPage = () => {
               <Link
                 target="_blank"
                 href={"/create-project"}
-                className="text-center bg-black hover:bg-white border-2 py-2 px-4 text-xl mt-8 hover:text-black  text-white border-white hover:border-transparent transition duration-200 hover:font-semibold"
+                className="text-center dark:bg-black bg-neutral-100 hover:border-white border-transparent hover:bg-black dark:hover:bg-white border-2 py-2 px-4 text-xl mt-8 hover:text-white dark:hover:text-black  dark:text-white text-black dark:border-white transition duration-200 font-semibold"
               >
                 Try now
               </Link>
@@ -204,7 +204,7 @@ const LandingPage = () => {
       <section className={`px-12 text-center py-32 flex flex-col items-center justify-center ${cormorant.className} `}>
         <p className="text-2xl md:text-3xl lg:text-4xl">Have something to say? We'd love to hear it!</p>
         <Link
-          className="text-center bg-black hover:bg-white border-2 py-2 px-4 text-xl mt-8 hover:text-black  text-white border-white hover:border-transparent transition duration-200 hover:font-semibold"
+                className="text-center dark:bg-black bg-neutral-100 hover:bg-black dark:hover:bg-white border-2 py-2 px-4 text-xl mt-8 hover:text-white dark:hover:text-black  dark:text-white text-black dark:border-white border-black transition duration-200 font-semibold"
           target="_blank"
           href={"mailto:aakifmohamed@elostack.com"}
         >
--- components/navbar/NavBar.jsx
+++ components/navbar/NavBar.jsx
@@ -35,7 +35,11 @@ const NavBar = () => {
       listenToNotifications();
     };
 
-    if (session && window.location.pathname !== "/signup" && window.location.pathname !== "/signin") {
+    if (
+      session &&
+      window.location.pathname !== "/signup" &&
+      window.location.pathname !== "/signin"
+    ) {
       if (session.data.session) {
         if (!dataLoaded) loadData();
       }
@@ -65,7 +69,10 @@ const NavBar = () => {
       const response = await fetch(`/api/notification/${userId}`, {
         method: "GET",
         headers: {
-          "X-Supabase-Auth": session.data.session.access_token + " " + session.data.session.refresh_token,
+          "X-Supabase-Auth":
+            session.data.session.access_token +
+            " " +
+            session.data.session.refresh_token,
         },
       });
       if (response.status === 200) {
@@ -99,7 +106,10 @@ const NavBar = () => {
           },
           (payload) => {
             console.log("New notification!");
-            setNotifications((prevNotifications) => [payload.new, ...prevNotifications]);
+            setNotifications((prevNotifications) => [
+              payload.new,
+              ...prevNotifications,
+            ]);
           }
         )
         .subscribe();
@@ -109,12 +119,23 @@ const NavBar = () => {
   };
 
   return (
-    <nav className={"min-h-16"}>
-      <div className={`px-8 lg:px-16 py-2 flex items-center justify-start ${isHomePage ? "max-lg:bg-black" : ""}`}>
-        <Link href={"/"} className={`${kanit.className} flex justify-center items-center text-2xl flex-shrink-0`}>
+    <nav className={`${(isHomePage && !session?.data.session) ? "text-white" : ""} min-h-16`}>
+      <div
+        className={`px-8 lg:px-16 py-2 flex items-center justify-start ${
+          (isHomePage && !session?.data.session) ? "max-lg:bg-black" : ""
+        }`}
+      >
+        <Link
+          href={"/"}
+          className={`${kanit.className} flex justify-center items-center text-2xl flex-shrink-0`}
+        >
           {currentTheme && (
             <Image
-              src={isHomePage || currentTheme === "dark" ? "/logo.png" : "/logo_black.png"}
+              src={
+                (isHomePage && !session?.data.session) || currentTheme === "dark"
+                  ? "/logo.png"
+                  : "/logo_black.png"
+              }
               alt={"logo"}
               width={50}
               height={50}
@@ -123,13 +144,13 @@ const NavBar = () => {
           <span className="max-lg:hidden">EloStack</span>
         </Link>
         <DesktopNav
+          isHomePage={isHomePage}
           notifications={notifications}
           setNotifications={setNotifications}
           showSignIn={showSignIn}
           session={session}
           currentTheme={currentTheme}
           toggleTheme={toggleTheme}
-          isHomePage={isHomePage}
           signOut={signOut}
         />
         <MobileNav
@@ -139,39 +160,51 @@ const NavBar = () => {
           showMobileDropdown={showMobileDropdown}
           currentTheme={currentTheme}
           toggleTheme={toggleTheme}
-          isHomePage={isHomePage}
           session={session}
         />
       </div>
       {showMobileDropdown && (
-        <MobileDropdown isHomePage={isHomePage} showSignIn={showSignIn} session={session} signOut={signOut} />
+        <MobileDropdown
+          isHomePage={isHomePage}
+          showSignIn={showSignIn}
+          session={session}
+          signOut={signOut}
+        />
       )}
     </nav>
   );
 };
 
 const DesktopNav = ({
+  isHomePage,
   notifications,
   setNotifications,
   showSignIn,
   session,
   currentTheme,
   toggleTheme,
-  isHomePage,
   signOut,
 }) => {
   return (
     <div className="ml-12 flex justify-start items-center gap-4 max-lg:hidden w-full">
       <Link
         href={"/projects"}
-        className="p-2 border-b-2 border-transparent hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+        className={`p-2 border-b-2 border-transparent  ${
+          (isHomePage && !session?.data.session)
+          ? "hover:border-b-gray-300 hover:text-gray-300"
+          : "hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+        }`}
       >
         Projects
       </Link>
       {session?.data.session && (
         <Link
-          href={"/my-projects"}
-          className="p-2 border-b-2 border-transparent hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+        href={"/my-projects"}
+        className={`p-2 border-b-2 border-transparent  ${
+            (isHomePage && !session?.data.session)
+            ? "hover:border-b-gray-300 hover:text-gray-300"
+            : "hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+          }`}
         >
           My Projects
         </Link>
@@ -179,19 +212,30 @@ const DesktopNav = ({
       <Link
         target="_blank"
         href={"https://discord.gg/PPbGuu3u43"}
-        className="p-2 border-b-2 border-transparent hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+        className={`p-2 border-b-2 border-transparent  ${
+          (isHomePage && !session?.data.session)
+            ? "hover:border-b-gray-300 hover:text-gray-300"
+            : "hover:text-gray-500 hover:border-b-gray-500 dark:hover:border-b-gray-300 dark:hover:text-gray-300"
+        }`}
       >
         Discord
       </Link>
 
       <div className="flex justify-center items-center ml-auto relative">
         {session?.data.session && (
-          <NotificationBell notifications={notifications} setNotifications={setNotifications} />
+          <NotificationBell
+            notifications={notifications}
+            setNotifications={setNotifications}
+          />
         )}
-        {!isHomePage && currentTheme && (
+        {currentTheme && (
           <button
             type="button"
-            onClick={() => (currentTheme == "dark" ? toggleTheme("light") : toggleTheme("dark"))}
+            onClick={() =>
+              currentTheme == "dark"
+                ? toggleTheme("light")
+                : toggleTheme("dark")
+            }
             className="mr-8 self-center text-xl"
           >
             {currentTheme === "light" ? <MdDarkMode /> : <MdLightMode />}
@@ -203,7 +247,7 @@ const DesktopNav = ({
           ) : (
             <Link
               href={"/signin"}
-              className="ml-4 hover:bg-gray-300 dark:hover:bg-neutral-600 px-3 py-1 rounded-full text-sm"
+              className={`${(isHomePage && !session?.data.session) ? "hover:bg-neutral-600 " : "hover:bg-gray-300 dark:hover:bg-neutral-600 "} ml-4  px-3 py-1 rounded-full text-sm`}
             >
               Sign In
             </Link>
@@ -220,22 +264,31 @@ const MobileNav = ({
   showMobileDropdown,
   currentTheme,
   toggleTheme,
-  isHomePage,
   session,
 }) => {
   return (
     <div className="flex items-center gap-2 ml-auto lg:hidden">
-      {!isHomePage && currentTheme && (
+      {currentTheme && (
         <button
           type="button"
-          onClick={() => (currentTheme == "dark" ? toggleTheme("light") : toggleTheme("dark"))}
+          onClick={() =>
+            currentTheme == "dark" ? toggleTheme("light") : toggleTheme("dark")
+          }
           className="mr-4 self-center text-xl"
         >
           {currentTheme === "light" ? <MdDarkMode /> : <MdLightMode />}
         </button>
       )}
-      {session?.data.session && <NotificationBell notifications={notifications} setNotifications={setNotifications} />}
-      <button type="button" onClick={() => setShowMobileDropdown(!showMobileDropdown)}>
+      {session?.data.session && (
+        <NotificationBell
+          notifications={notifications}
+          setNotifications={setNotifications}
+        />
+      )}
+      <button
+        type="button"
+        onClick={() => setShowMobileDropdown(!showMobileDropdown)}
+      >
         <IoMenu className="text-2xl" />
       </button>
     </div>
@@ -244,12 +297,22 @@ const MobileNav = ({
 
 const MobileDropdown = ({ isHomePage, showSignIn, session, signOut }) => {
   return (
-    <div className={`${isHomePage ? "bg-black" : ""} flex flex-col justify-center items-center lg:hidden py-2`}>
-      <Link href={"/projects"} className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center">
+    <div
+      className={`${
+        (isHomePage && !session?.data.session) ? "bg-black" : ""
+      } flex flex-col justify-center items-center lg:hidden py-2`}
+    >
+      <Link
+        href={"/projects"}
+        className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center"
+      >
         Projects
       </Link>
       {session?.data.session && (
-        <Link href={"/my-projects"} className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center">
+        <Link
+          href={"/my-projects"}
+          className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center"
+        >
           My Projects
         </Link>
       )}
@@ -262,13 +325,20 @@ const MobileDropdown = ({ isHomePage, showSignIn, session, signOut }) => {
       </Link>
       <hr className="border-0 h-[1px] bg-gray-400 my-2 w-full" />
       {session?.data.session && (
-        <Link href={"/account-settings"} className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center">
+        <Link
+          href={"/account-settings"}
+          className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center"
+        >
           My Account
         </Link>
       )}
       {showSignIn && (
         <div className="p-2 hover:bg-gray-300 dark:hover:bg-neutral-600 w-full text-center">
-          {session?.data.session ? <button onClick={signOut}>Log out</button> : <Link href={"/signin"}>Sign In</Link>}
+          {session?.data.session ? (
+            <button onClick={signOut}>Log out</button>
+          ) : (
+            <Link href={"/signin"}>Sign In</Link>
+          )}
         </div>
       )}
     </div>
